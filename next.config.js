c4b05739095d40913f73a50fe3864a3a75fa69a5
TayMcQuaya <taymcquaya@gmail.com>
2025-01-23 10:29:47 +0800

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Auth-LogIn',
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_ID: process.env.GOOGLE_ID,
